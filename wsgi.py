d592a984f15cd15f1b83d053ef85cd9c837a7acc
K ANIL KUMAR <anilkumarkedarsetty@gmail.com>
2025-02-08 15:37:54 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,5 @@
-# from app import app
+from app import app
 
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=10000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
 
-import os
-import sys
-
-# Add your project directory to path
-path = '/home/AnilKumarK/mysite'
-if path not in sys.path:
-    sys.path.append(path)
-
-from app import app as application
